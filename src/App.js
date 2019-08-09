e1af44750c1fe7646c323651995cbe93df8c5182
d.s.pustovit <d.s.pustovit@m9.etagi.com>
2019-08-09 13:03:06 +0500

    config for router

--- src/App.js
+++ src/App.js
@@ -3,16 +3,27 @@ import { Router } from 'preact-router';
 import AsyncRoute from 'preact-async-route';
 import Header from 'components/Header/Header';
 
-const getHomeLayout = () => {
-  return import(/* webpackChunkName: "HomeLayout" */ 'layouts/HomeLayout').then(m => m.default)
-};
+const routes = [
+  {
+    path: '/',
+    getComponent: () => {
+      return import(/* webpackChunkName: "HomeLayout" */ 'layouts/HomeLayout').then(m => m.default)
+    }
+  }
+]
 
-function App() {
+const getAsyncRoutes = () => {
+  return routes.map(route =>
+    <AsyncRoute path={route.path} getComponent={route.getComponent}/>
+  );
+}
+
+const App = url => {
   return (
     <div>
       <Header />
-      <Router>
-        <AsyncRoute path='/' getComponent={getHomeLayout}/>
+      <Router {...(url || {})}>
+        {getAsyncRoutes()}
       </Router>
     </div>
   );
